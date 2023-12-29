7d51c4d60b2adf223290a4046d7df1cc899b2732
hesham <152308429+Hesham-AI@users.noreply.github.com>
2023-12-29 02:11:46 +0200

    first commit

---
+++ js/welcom.js
@@ -0,0 +1,26 @@
+
+var logOut=document.getElementById("logOut");
+var userWelcom=document.getElementById("userWelcom");
+var userName=localStorage.getItem("userName")
+var collection=[];
+
+if(localStorage.getItem("collects")!=null){
+collection=JSON.parse(localStorage.getItem("collects") )
+}
+
+if(userName){
+    userWelcom.innerHTML="Welcome " + userName;
+
+}else{
+    userWelcom.innerHTML="you should login !!"
+}
+
+
+logOut.addEventListener("click", function(){
+
+localStorage.removeItem("userName")
+
+
+window.location="./index.html"
+
+} )
